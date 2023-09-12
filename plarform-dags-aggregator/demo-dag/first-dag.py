e52c674eca7cf11996ab09169a77f92091a99699
ashis34 <88176690+ashis34@users.noreply.github.com>
2023-09-12 12:41:32 +0530

    Update first-dag.py

--- plarform-dags-aggregator/demo-dag/first-dag.py
+++ plarform-dags-aggregator/demo-dag/first-dag.py
@@ -12,8 +12,8 @@ with DAG(
     dag_id='our_first_dag',
     default_args=default_args,
     description='This is our first dag',
-    start_date=datetime(2023, 9, 12, 2),
-    schedule_interval='@daily'
+    #start_date=datetime(2023, 9, 12, 2),
+    #schedule_interval='@daily'
 ) as dag: 
     task1=BashOperator(
         task_id='first_task',
