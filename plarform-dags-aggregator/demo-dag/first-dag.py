d9460c4e621560bf11143a2d565a43f0e1db1b23
ashis34 <88176690+ashis34@users.noreply.github.com>
2023-10-26 17:11:11 +0530

    Update first-dag.py

--- plarform-dags-aggregator/demo-dag/first-dag.py
+++ plarform-dags-aggregator/demo-dag/first-dag.py
@@ -19,7 +19,7 @@ with DAG(
     #schedule_interval='@daily'
     access_control={
         'role_ashis': {
-            'can_edit'
+            'can_delete'
         },
         'role_shashank': {
             'can_read',
