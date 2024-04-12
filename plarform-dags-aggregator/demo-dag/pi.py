5f9e5dbe1ae37d465f679c035f710d76c30cf4a0
renuka-p1 <166815892+renuka-p1@users.noreply.github.com>
2024-04-12 22:23:52 +0530

    Update pi.py

--- plarform-dags-aggregator/demo-dag/pi.py
+++ plarform-dags-aggregator/demo-dag/pi.py
@@ -1,45 +1,56 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+from airflow import DAG
+from airflow.models.param import Param
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
+    SparkKubernetesSensor,
+)
+from airflow.utils.dates import days_ago
 
-import sys
-from random import random
-from operator import add
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "max_active_runs": 1,
+    "retries": 0,
+}
 
-from pyspark.sql import SparkSession
+dag = DAG(
+    "spark_pi",
+    default_args=default_args,
+    schedule_interval=None,
+    tags=["ezaf", "spark", "pi"],
+    params={
+        "airgap_registry_url": Param(
+            "",
+            type=["null", "string"],
+            pattern=r"^$|^\S+/$",
+            description="Airgap registry url. Trailing slash in the end is required",
+        ),
+    },
+    render_template_as_native_obj=True,
+    access_control={"All": {"can_read", "can_edit", "can_delete"}},
+)
 
+submit = SparkKubernetesOperator(
+    task_id="submit",
+    application_file="example_spark_pi.yaml",
+    do_xcom_push=True,
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    enable_impersonation_from_ldap_user=True,
+)
 
-if __name__ == "__main__":
-    """
-        Usage: pi [partitions]
-    """
-    spark = SparkSession\
-        .builder\
-        .appName("PythonPi")\
-        .getOrCreate()
+sensor = SparkKubernetesSensor(
+    task_id="monitor",
+    application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    attach_log=True,
+)
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
-
-    def f(_: int) -> float:
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
-
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
-
-    spark.stop()
+submit >> sensor
