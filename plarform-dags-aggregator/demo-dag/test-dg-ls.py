62382f6021d59fa9d129b6106d76495f58795328
renuka-p1 <166815892+renuka-p1@users.noreply.github.com>
2024-04-22 14:43:40 +0530

    Create test-dg-ls.py

---
+++ plarform-dags-aggregator/demo-dag/test-dg-ls.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='our_first_dag',
+    default_args=default_args,
+    description='This is our first dag',
+    start_date= days_ago(2),
+    schedule_interval=None,
+    #start_date=datetime(2023, 9, 12, 2),
+    #schedule_interval='@daily'
+    access_control={
+        'role_ashis': {
+            'can_read'
+        },
+        'role_shashank': {
+            'can_read',
+            'can_edit',
+            'can_delete'
+        },
+        'role_hpedemo-user01': {
+            'can_read'
+        },
+        'role_demo-admin': {
+            'can_read',
+            'can_delete'
+        },
+        'role_rohit': {
+            'can_read'
+        }
+}
+) as dag: 
+    task1=BashOperator(
+        task_id='first_task',
+        bash_command="ls /mnt/datasources/alletra/alletraalloyd-pvc-1/"
+    )
+
+    task1
